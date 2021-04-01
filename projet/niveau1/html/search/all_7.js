49774c1261da9034f24a0bd6889093e1dc9b94ba
LESNIAK Louis <louis.lesniak4@ent.univ-lorraine.fr>
2021-04-01 18:11:47 +0200

    Niveau 1 terminé + ajout de la documentation Doxygen

---
+++ projet/niveau1/html/search/all_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['ligne_5farrive_22',['ligne_arrive',['../structtextures__s.html#a6054c7a22e40906b9f26a467851bca91',1,'textures_s::ligne_arrive()'],['../structworld__s.html#a74121224ab6c5bbaf44034029358bda3',1,'world_s::ligne_arrive()']]],
+  ['load_5fimage_23',['load_image',['../sdl2-light_8c.html#a85a3dc2aec2bf7ee942b5761ed9e4b35',1,'load_image(const char path[], SDL_Renderer *renderer):&#160;sdl2-light.c'],['../sdl2-light_8h.html#a85a3dc2aec2bf7ee942b5761ed9e4b35',1,'load_image(const char path[], SDL_Renderer *renderer):&#160;sdl2-light.c']]]
+];
